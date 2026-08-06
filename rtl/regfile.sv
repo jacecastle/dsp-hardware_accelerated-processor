6eb9e0d32d0ec69068855a939e50e3655b115097
Nitya Vergis <nitya.anish@gmail.com>
2026-08-05 23:04:06 -0500

    Add Regfile module

---
+++ rtl/regfile.sv
@@ -0,0 +1,32 @@
+module reg_file(input logic clk,
+                input logic reset,
+                input logic [31:0] wr_data,
+                input logic [4:0] RS1,
+                input logic [4:0] RS2,
+                input logic [4:0] RD,
+                input logic ld_reg, 
+                output logic [31:0] RS1_out,
+                output logic [31:0] RS2_out
+    );
+
+     logic [31:0] reg_file [32];
+     integer i;
+    
+     assign RS1_out = reg_file[RS1];
+     assign RS2_out = reg_file[RS2];
+
+         
+    always_ff@(posedge clk)begin
+        if(reset) begin
+            for(i=0; i<=31; i=i+1) begin
+                reg_file[i] <= 32'h0000;
+            end
+        end
+        else if(ld_reg)begin
+            reg_file[RD]<=wr_data;    
+        end
+    end
+            
+endmodule
+.a(RS1),
+
