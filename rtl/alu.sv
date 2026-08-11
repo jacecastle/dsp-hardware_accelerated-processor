f821001715860cf3ab0219c7e1e98ce3facf5c18
Nitya Vergis <nitya.anish@gmail.com>
2026-08-11 13:30:40 -0500

    Implement initial ALU and R-type decoder

--- rtl/alu.sv
+++ rtl/alu.sv
@@ -20,7 +20,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module ALU(input logic [31:0] RS1, RS2,
+module ALU(input logic [31:0] rs1, rs2,
            input logic [2:0] ALUK,
            output logic [31:0] alu_out
 
@@ -28,11 +28,11 @@ module ALU(input logic [31:0] RS1, RS2,
     always_comb begin
       
         unique case (ALUK)
-            3'b000: alu_out = RS1 + RS2;
-            3'b001: alu_out = RS1 & RS2;
-            3'b010: alu_out = RS1 + (~RS2+32'h0001);
-            3'b011: alu_out = RS1 ^ RS2;
-            3'b100: alu_put = RS1 || RS2;
+            3'b000: alu_out = rs1 + rs2;
+            3'b001: alu_out = rs1 & rs2;
+            3'b010: alu_out = rs1 + (~rs2+32'h0001);
+            3'b011: alu_out = rs1 ^ rs2;
+            3'b100: alu_put = rs1 || rs2;
             default : alu_out = 32'h0000;
          endcase
     end
--- rtl/decoder.sv
+++ rtl/decoder.sv
@@ -1,3 +1,47 @@
-module decoder(input logic [6:0] opcode
-
+module decoder(input logic [31:0] instruction,
+               output logic [2:0] ALUK,
+               output logic RegWr, 
+               output logic [4:0] rs1, rs2, rd
 );
+
+logic [6:0] opcode = instruction[6:0];
+logic [2:0] funct3 = instruction[14:12];
+logic [6:0] funct7 = instruction[31:25];
+
+
+always_comb begin
+    RegWr = 1'b0;
+    ALUK = 3'b0;
+    
+    case(opcode)
+
+    //rtype
+    7'b0110011: begin
+    assign rs1 = instruction[19:15];
+    assign rs2 = instruction[24:20];
+    assign rd  = instruction[11:7];
+        RegWr = 1'b1;
+        case(funct3)
+            3'b000: begin
+                if(funct7==7'b0) 
+                    ALUK = 3'b000; //add
+                else if (funct7 == 7'b0100000)
+                    ALUK =   3'b001; //sub
+            end
+
+            3'b111:
+                ALUK = 3'b010; //and
+
+
+        endcase
+    end
+
+    endcase  
+    
+    
+
+
+end
+
+
+endmodule
